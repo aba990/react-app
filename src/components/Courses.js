018b229ac4d2803be1c9601ce2c56279d9673b91
AnelB <anel.b@live.com>
2022-05-26 23:09:01 +0200

    up up

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,5 +1,32 @@
+
 const Courses = () => {
-    return <h1>Courses</h1>
+    return (
+        <div className='container'>
+            <h1>Courses</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Course Name</th>
+                        <th>Points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Name of the course</td>
+                        <td>Points for the coruse</td>
+                    </tr>
+                    <tr>
+                        <td>Name of the course</td>
+                        <td>Points for the coruse</td>
+                    </tr>
+                    <tr>
+                        <td>Name of the course</td>
+                        <td>Points for the coruse</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Courses;
