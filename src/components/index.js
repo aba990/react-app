00458bf9239257fa8766efad559e9157c67cc6ad
AnelB <anel.b@live.com>
2022-05-27 22:53:56 +0200

    up up

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import AppFooter from './AppFooter';
 import AppHeader from './AppFooter';
 import Courses from './Courses';
+import Course from './Course';
 import Home from './Home';
 import Students from './Students';
 
-export { AppFooter, AppHeader, Courses, Home, Students };
+export { AppFooter, AppHeader, Courses, Course, Home, Students };
